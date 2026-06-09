139fa69fd39418e3d8b8ffe22c75701d002e77b1
mansigarg27 <mansigarg27april@gmail.com>
2026-06-10 00:02:29 +0530

    Add files via upload

---
+++ to-do_list.py/to-do_list.py
@@ -0,0 +1,31 @@
+# To-Do List Application
+
+tasks = []
+
+while True:
+    print("TO-DO LIST")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        task = input("Enter a task: ")
+        tasks.append(task)
+        print("Task added successfully!")
+
+    elif choice == "2":
+        if len(tasks) == 0:
+            print(" No tasks available.")
+        else:
+            print("\nYour Tasks:")
+            for index, task in enumerate(tasks, start=1):
+                print(f"{index}. {task}")
+
+    elif choice == "3":
+        print("Thank you for visiting!")
+        break
+
+    else:
+        print(" Invalid choice. Please enter 1, 2, or 3.")
